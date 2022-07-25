c494e2b9f0cd0ef95c0fec4a79478c8a57ad7d4b
ujjwalgarg100204 <ujjwalgarg100204@gmail.com>
2022-07-25 23:51:24 +0530

    Initial Commit

---
+++ src/main/java/July_25/EscapeSequences.java
@@ -0,0 +1,9 @@
+package July_25;
+
+public class EscapeSequences {
+    public static void main(String[] args) {
+        String firstName = "Ujjwal";
+        String lastName = "Garg";
+        System.out.printf("Welcome \n\t%s %s\n", firstName, lastName);
+    }
+}
